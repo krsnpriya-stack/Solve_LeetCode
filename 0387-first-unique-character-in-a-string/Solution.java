6007aed173d7c4d3224d6bd38412996278231175
krsnpriya <258563566+krsnpriya-stack@users.noreply.github.com>
2026-02-27 23:50:34 +0530

    Update Solution.java

--- 0387-first-unique-character-in-a-string/Solution.java
+++ 0387-first-unique-character-in-a-string/Solution.java
@@ -5,6 +5,7 @@ class Solution {
             boolean isUnique = true;
 
             for (int j = 0; j < s.length(); j++) {
+                
                 if (i != j && s.charAt(i) == s.charAt(j)) {
                     isUnique = false;
                     break;
